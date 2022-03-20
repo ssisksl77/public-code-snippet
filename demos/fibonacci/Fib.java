9484e25747c72133ff5cf87b60a8e204b669ef68
ssisksl77 <ssiskl77@gmail.com>
2022-03-21 00:37:31 +0900

    first-commit

---
+++ demos/fibonacci/Fib.java
@@ -0,0 +1,22 @@
+public class Fib {
+    private final int n;
+    public Fib(int n) {
+        this.n = n;
+    }
+
+    public int get() {
+        if (this.n <= 0) return 0;
+        if (this.n == 1) return 1;
+
+        int n1 = 0;
+        int n2 = 1;
+        int cnt = 1;
+        while(cnt <= this.n) {
+            int tmp = n2;
+            n2 += n1;
+            n1 = tmp;
+            cnt++;
+        }
+        return n1;
+    }
+}
---
+++ demos/fibonacci/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        System.out.println(new Fib(n).get());
+    }
+}
